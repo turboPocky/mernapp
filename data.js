d5d1cd5fb8d6499a8130242b1d64f2d4a32477a4
turboPocky <turboPocky@gmail.com>
2017-01-21 02:58:44 -0600

    initial implementation

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+{ id: 1, author: 'Pocky', text: `it's working!`},
+{id:2, author: 'You', text : `you're __right!__`}
+]
+
+module.exports = data;
---
+++ src/commentbox.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import CommentList from './commentlist';
+import CommentForm from './commentform';
+import style from './style';
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  { data: [] };
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    this.handleCommentDelete = this.handleCommentDelete.bind(this);
+    this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
+  }
+
+  loadCommentsFromServer() {
+    axios.get(this.props.url)
+      .then(res => {
+        this.setState({data: res.data});
+      })
+    }
+
+  handleCommentSubmit(comment) {
+    let comments = this.state.data;
+    comment.id=Date.now();
+    let newComments = comments.concat(comment);
+    this.setState({data: newComments});
+
+    axios.post(this.props.url, comment)
+      .then(res => {
+      })
+      .catch(err => {
+        this.setState({data: comments});
+        console.error(err);
+      })
+  }
+
+  handleCommentDelete(id) {
+    axios.delete(`${this.props.url}/${id}`)
+      .then(res => {
+        console.log('comment deleted');
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
+  handleCommentUpdate(id, comment) {
+    axios.put(`${this.props.url}/${id}`, comment)
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  }
+
+  render() {
+    return (
+      <div style={ style.commentBox }>
+        <h2>Comments:</h2>
+        <CommentList
+          onCommentDelete={this.handleCommentDelete}
+          onCommentUpdate={this.handleCommentUpdate}
+          data={this.state.data}/>
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+      </div>
+     )
+   }
+}
+
+export default CommentBox;
